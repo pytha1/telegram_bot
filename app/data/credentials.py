76c0fa97f2bae513611f2ccb5c4e09cf29ca0beb
pytha1 <101905859+pytha1@users.noreply.github.com>
2023-04-03 16:37:57 +0200

    Update credentials.py

--- app/data/credentials.py
+++ app/data/credentials.py
@@ -2,5 +2,5 @@ bot_token = "6192491084:AAG8cfhwdyE13m91Y4QAzKWyAqbWfkOwA9I"
 auth_code = "123456"  # set it yourself
 admin_code = "246810"  # set it yourself
 ip = "127.0.0.1"
-db_user = "AdminLass"
-db_pw = "bdlasspass"
+db_user = "admin"
+db_pw = "Lassbd"
